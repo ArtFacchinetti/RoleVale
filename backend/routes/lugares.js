98aab582787e59498abcbd67f36f5e15310fbc8c
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-06-27 16:49:35 -0300

    Data can be parsed from data.json

--- backend/routes/lugares.js
+++ backend/routes/lugares.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import fs from 'fs';
 
+import {gerarHTMLLugar, tagRendering} from '../render.js';
+
 const router = express.Router();
 
+function getData() {
+    try {
+        const data = fs.readFileSync('./backend/data/data.json','utf8');
+        return JSON.parse(data)
+    }
+    catch (err) {
+        console.log('Erro ao carregar dados: ', err)
+     }
+}
+
+const data = getData()
+
 router.get('/', (req, res) => {
-    res.send('Lugares legais d+')
+    res.send()
 })
 
 export default router;
