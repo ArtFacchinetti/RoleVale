a81b43e5c18cc545e71a372e7e3fb3160f95518b
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-06-28 01:04:54 -0300

    Fetch and export data from API

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
-import {gerarHTMLLugar, tagRendering} from '../render.js';
+// import { gerarHTMLLugar, tagRendering } from '../render.js';
 
 let data;
 
 async function fetchData() {
     try {
-    const raw = await fetch('http://localhost:3000/lugares');
-    const json = await raw.json();
-    data = json;
-    console.log(data)
+        const raw = await fetch('http://localhost:3000/lugares');
+        const json = await raw.json();
+        data = json;
+        console.log(data)
     }
     catch (err) {
         console.error('Erro ao tentar dar Fetch: ', err)
@@ -15,3 +15,4 @@ async function fetchData() {
 }
 await fetchData();
 
+export default data;
